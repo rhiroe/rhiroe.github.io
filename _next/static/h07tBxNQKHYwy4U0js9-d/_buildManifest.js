3ac034009738518dd91bfc481a0640da4507f03e
rhiroe <rhiroe@users.noreply.github.com>
2022-06-15 06:12:50 +0000

    deploy: 480b3b31db5dc6ed0a91c537a4f03d0d4491bbda

--- _next/static/h07tBxNQKHYwy4U0js9-d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d9babefee07fc5e7.css","static/chunks/pages/index-e9381f37e8a454b0.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
