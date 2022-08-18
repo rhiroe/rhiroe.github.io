28d53056325ee1a4611013c630fdde00445c2405
rhiroe <ride.poke@gmail.com>
2022-08-18 22:02:01 +0900

    共通部分のデザイン修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,69 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Fab from '@mui/material/Fab';
+import Container from '@mui/material/Container';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
       <>
-        <Component {...pageProps} />
-        <footer className="footer">
-          <div className="float row">
-            <a
-                href={"mailto:ride.poke@gmail.com"}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <img width="32px" src="/mail.svg" alt={"Icon of gmail"} />
-            </a>
-            <a
-                href={"https://github.com/rhiroe"}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <img width="32px" src="/github.svg" alt={"Icon of twitter"} />
-            </a>
-            <a
-                href={"https://twitter.com/messages/compose?recipient_id=509745934"}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <img width="32px" src="/twitter.svg" alt={"Icon of twitter"} />
-            </a>
-          </div>
-          <div className="row">
-            © 2022 rhiroe
-          </div>
-        </footer>
+          <AppBar position="static">
+              <Toolbar>
+                  <a href="https://github.com/rhiroe">
+                      <Avatar sx={{ mr: 2 }} alt="rhiroe" src="/rhiroe_icon.jpg" />
+                  </a>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                      {'Ryosuke "Rio" Hiroe'}
+                  </Typography>
+                  <Fab
+                      size="small"
+                      sx={{ mr: 1 }}
+                      color="primary"
+                      aria-label="add"
+                      href="mailto:ride.poke@gmail.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      <EmailIcon />
+                  </Fab>
+                  <Fab
+                      size="small"
+                      sx={{ mr: 1 }}
+                      color="primary"
+                      aria-label="add"
+                      href="https://github.com/rhiroe"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      <GitHubIcon />
+                  </Fab>
+                  <Fab
+                      size="small"
+                      sx={{ mr: 1 }}
+                      color="primary"
+                      aria-label="add"
+                      href="https://twitter.com/messages/compose?recipient_id=509745934"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      <TwitterIcon />
+                  </Fab>
+              </Toolbar>
+          </AppBar>
+          <Container maxWidth="md">
+              <Component {...pageProps} />
+          </Container>
+          <footer className="footer">
+              <div className="row">
+                  © 2022 rhiroe
+              </div>
+          </footer>
       </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,54 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '~/styles/Home.module.css'
-import PageTitle from "~/components/common/pageTitle";
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>{'Ryosuke "Rio" Hiroe'}</title>
-        <meta name="description" content="My page." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <>
+        <Head>
+          <title>{'Ryosuke "Rio" Hiroe'}</title>
+          <meta name="description" content="My page." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <PageTitle>{'Ryosuke "Rio" Hiroe'}</PageTitle>
-
-        <img className={styles.thumbnail} src={'/rhiroe_icon.jpg'} alt={"Icon of rhiroe"} />
-
-        <p className={styles.description}>
-          岡山でRuby書いたりTypeScript書いたりしてます。
-        </p>
-
-        <div className={styles.grid}>
-          <Link href="blog">
-            <a className={styles.card}>
-              <h2>Blog &rarr;</h2>
-              <p>プログラミングの話</p>
-            </a>
-          </Link>
-
-          <Link href="english">
-            <a className={styles.card}>
-              <h2>English &rarr;</h2>
-              <p>英語学習の記録</p>
-            </a>
-          </Link>
-        </div>
-        <div className={styles.grid}>
-          <a
-            href="https://gist.github.com/rhiroe/62a5f0dbd9c44bbf539f6effc4ef5514"
-            className={styles.card}
-          >
-            <h2>Profile &rarr;</h2>
-            <p>職務経歴とか</p>
-          </a>
-
-          {/*<a*/}
-          {/*  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-          {/*  className={styles.card}*/}
-          {/*>*/}
-          {/*  <h2>Deploy &rarr;</h2>*/}
-          {/*  <p>*/}
-          {/*    Instantly deploy your Next.js site to a public URL with Vercel.*/}
-          {/*  </p>*/}
-          {/*</a>*/}
-        </div>
-      </main>
-    </div>
+        <main>
+          <p>
+            岡山でRuby書いたりTypeScript書いたりしてます。
+          </p>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Paper>
+                <Link href="blog">
+                  <a>
+                    <h2>Blog &rarr;</h2>
+                    <p>プログラミングの話</p>
+                  </a>
+                </Link>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>
+                <Link href="english">
+                  <a>
+                    <h2>English &rarr;</h2>
+                    <p>英語学習の記録</p>
+                  </a>
+                </Link>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>
+                <a href="https://gist.github.com/rhiroe/62a5f0dbd9c44bbf539f6effc4ef5514">
+                  <h2>Profile &rarr;</h2>
+                  <p>職務経歴とか</p>
+                </a>
+              </Paper>
+            </Grid>
+          </Grid>
+        </main>
+      </>
   )
 }
 
